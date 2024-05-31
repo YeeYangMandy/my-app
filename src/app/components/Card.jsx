89ab432a46c468af698c507e6478baf999063405
Yi Yang <oprtigk@gmail.com>
2024-05-31 16:36:09 +0800

    自訂義Card

---
+++ src/app/components/Card.jsx
@@ -0,0 +1,11 @@
+/**
+* @author Yang, yi <oprtigk@gmail.com>
+*/
+
+export default function CustomCard({item}){
+    return[
+        <div class="border">
+            <div>{item.ScenicSpotName}</div>
+        </div>
+    ];
+};
